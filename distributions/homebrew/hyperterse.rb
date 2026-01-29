f842ac53bfdbbd956542e7a9b703abc5b82ccf17
Samrith Shankar <samrith.298@gmail.com>
2026-01-29 10:02:31 +0530

    Update distribution manifests to v1.1.0

--- distributions/homebrew/hyperterse.rb
+++ distributions/homebrew/hyperterse.rb
@@ -8,7 +8,7 @@
 class Hyperterse < Formula
   desc "A declarative interface to connect your database to your AI agents"
   homepage "https://hyperterse.com"
-  version "1.0.0"
+  version "1.1.0"
   license "Apache-2.0"
 
   on_macos do
